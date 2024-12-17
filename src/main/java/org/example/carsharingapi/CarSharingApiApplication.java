4704f0079f9e5d62f8a6934f204da9493b4749b4
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-12-17 15:24:24 +0200

    Initial commit

---
+++ src/main/java/org/example/carsharingapi/CarSharingApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.carsharingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingApiApplication.class, args);
+    }
+
+}
